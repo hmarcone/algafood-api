2db6e8156e9bf3a008c2f3d421b73a03a1858341
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-11-08 15:21:43 -0300

    03.20-desafio-mapeando-entidades

--- src/main/java/com/algaworks/algafood/domain/model/Estado.java
+++ src/main/java/com/algaworks/algafood/domain/model/Estado.java
@@ -5,13 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Cozinha {
+public class Estado {
 
 	@EqualsAndHashCode.Include
 	@Id
@@ -20,4 +21,5 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+	
 }
--- src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -5,13 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Cozinha {
+public class FormaPagamento {
 
 	@EqualsAndHashCode.Include
 	@Id
@@ -19,5 +20,6 @@ public class Cozinha {
 	private Long id;
 	
 	@Column(nullable = false)
-	private String nome;
+	private String descricao;
+	
 }
