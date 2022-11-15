8442cd84ce38e1580062739bfb7cd9dd96b73f78
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-11-15 18:32:00 -0300

    07.10.adicionando-dados-de-testes-com-callback-do-flyway

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,8 +1,7 @@
 package com.algaworks.algafood.infrastructure.repository;
 
-import static com.algaworks.algafood.domain.repository.spec.RestauranteSpecs.comFreteGratis;
-import static com.algaworks.algafood.domain.repository.spec.RestauranteSpecs.comNomeSemelhante;
-
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
