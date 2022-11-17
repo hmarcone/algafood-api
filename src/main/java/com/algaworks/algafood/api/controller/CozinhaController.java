a06bd59f8f23d4574b74f87e992d3dc055ae41c2
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-11-17 09:34:38 -0300

    08.06-desafio-refatorando-os-servicos-rest

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -55,21 +55,6 @@ public class CozinhaController {
 		return cadastroCozinha.salvar(cozinhaAtual);
 	}
 	
-//	@DeleteMapping("/{cozinhaId}")
-//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
-//		try {
-//			cadastroCozinha.excluir(cozinhaId);	
-//			return ResponseEntity.noContent().build();
-//			
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.notFound().build();
-//			
-//		} catch (EntidadeEmUsoException e) {
-//			return ResponseEntity.status(HttpStatus.CONFLICT)
-//					.body(e.getMessage());
-//		}
-//	}
-	
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cozinhaId) {
