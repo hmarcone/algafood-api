2d6febdf89b4f6574d17757efc3bd3f47ea5652c
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-11-21 15:04:03 -0300

    10.09-validando-o-corpo-da-resposta-http

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,4 +36,32 @@ public class CadastroCozinhaIT {
 			.statusCode(HttpStatus.OK.value());
 	}
 
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4));
+//			.body("nome", hasItems("Indiana", "Tailandesa"));
+	}	
+	
+	@Test
+	public void deveConterCozinhaTailandesa_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("nome", hasItems("Indiana", "Tailandesa"));
+	}		
 }
